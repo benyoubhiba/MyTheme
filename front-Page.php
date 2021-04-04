3cdd25be9672eb661361f716328867b617878c1e
benyoubhiba <77007049+benyoubhiba@users.noreply.github.com>
2021-04-05 01:00:32 +0200

    Add files via upload

---
+++ front-Page.php
@@ -0,0 +1,111 @@
+<?php get_header(); ?>
+
+	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+    
+    
+    
+    	<?php the_content(); ?>
+      
+
+
+      <div class="contenu2">
+        <div class="titel"> <h3>Prendre soin de notre santé mentale</h3>
+      </div>
+       
+        <P style=" font-size: 14pt;" class="col-sm-9 offset-sm-3">Prendre soin de sa santé psychique est aussi important que de s’occuper de sa santé physique. Des habitudes au quotidien permettent de l’améliorer ou de la maintenir à un niveau de bien-être agréable.
+
+          Il s’agit de cultiver un état d’esprit  et de pratiquer des activités qui favorisent la santé mentale.  Renforcer ses ressources psychiques personnelles (=ses atouts et ses forces) aide à mieux faire face aux périodes critiques qui peuvent survenir dans la vie. 
+          
+          Les éléments suivants ne constituent pas un « livre de recettes pour aller bien ». Il s’agit de quelques pistes qui contribuent au bien-être psychique. </P>
+  
+        
+        </div>
+   <div class="contenu3">
+
+    <div class="w3-content w3-display-container">
+      <img class="mySlides" src="<?php echo get_template_directory_uri(); ?> /img/vignettes_site_2_fr-1.png" style="width:40%">
+      <img class="mySlides" src="<?php echo get_template_directory_uri(); ?> /img/vignettes_site_3_fr-1.png" style="width:40%">
+      <img class="mySlides" src="<?php echo get_template_directory_uri(); ?> /img/vignettes_site_4_fr-1.png" style="width:40%">
+      <img class="mySlides" src="<?php echo get_template_directory_uri(); ?> /img/vignettes_site_5_fr-1.png" style="width:40%"></div>  
+    </div>
+     <div class="btn">   
+     
+    <button style="border-radius: 27px;color: brown;-webkit-box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, 48px 50px 50px -28px rgba(0,0,0,0), inset 48px 17px 34px -30px rgba(25,18,21,0.1); 
+    box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, 48px 50px 50px -28px rgba(0,0,0,0), inset 48px 17px 34px -30px rgba(25,18,21,0.1); position: absolute; top: 900px;left: 17%;" class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+      <button  style="border-radius: 27px;color: brown;-webkit-box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, 48px 50px 50px -28px rgba(0,0,0,0), inset 48px 17px 34px -30px rgba(25,18,21,0.1); 
+      box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, 48px 50px 50px -28px rgba(0,0,0,0), inset 48px 17px 34px -30px rgba(25,18,21,0.1); position: absolute; top: 900px;left: 68%;" class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+    
+
+    
+  </div>
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card" style="margin: 5%; -webkit-box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, inset 48px -6px 28px -30px rgba(25,18,21,0); 
+      box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, inset 48px -6px 28px -30px rgba(25,18,21,0);">
+     
+        <div class="card-body">
+          <h5 class="card-title">Prendre soin de soi et de son corps</h5>
+          <p class="card-text">Prendre soin de soi est quelque chose de très personnel. Les moyens sont nombreux et doivent être cherchés pour chacun individuellement. L’activité physique est un bon moyen de combattre les tensions. Il peut s’agir de pratiquer un sport ou simplement se balader pour s’aérer l’esprit. L’important étant de bouger, de sortir de chez soi, de changer d’air. 
+
+            Les phases d’activités peuvent être suivies de temps de détente. Ces moments de repos permettent de prendre du recul pour mieux réfléchir et apprécier le vécu de la journée. Bien que le sommeil soit important, une sortie, seul ou en groupe, favorise aussi la détente. Une sexualité épanouie ou une spiritualité bien vécue peuvent aussi être bénéfiques à la santé psychique..</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card" style="margin: 5%; -webkit-box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, inset 48px -6px 28px -30px rgba(25,18,21,0); 
+      box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, inset 48px -6px 28px -30px rgba(25,18,21,0);">
+        <div class="card-body">
+          <h5 class="card-title">Cultiver sa créativité</h5>
+          <p class="card-text">L’imagination et les compétences présentes en chacun ne demandent parfois qu’à s’exprimer ou à être développées. Il n’est jamais trop tard pour se lancer et découvrir un nouveau passe-temps ou reprendre une formation.
+
+            Les activités créatrices permettent d’exprimer des émotions positives ou négatives autrement que par la parole. Cela permet de libérer des tensions ou des soucis.Apprendre de nouvelles choses et développer ses compétences stimule l’esprit, renforce l’estime de soi et casse la routine quotidienne. Cela peut également s’avérer utile sur le plan professionnel, par exemple. 
+
+            Les cantons, les communes et les villes sont riches d’activités culturelles, artistiques, sportives ou d’offres de formations pour adultes. </p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card"  style="margin: 5%;-webkit-box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, inset 48px -6px 28px -30px rgba(25,18,21,0); 
+      box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000, inset 48px -6px 28px -30px rgba(25,18,21,0);">
+     
+        <div class="card-body">
+          <h5 class="card-title">Tisser et cultiver des liens autour de soi</h5>
+          <p class="card-text">L’être humain est un être social qui se réalise aussi dans ses liens avec les autres. Certaines personnes ont davantage besoin de liens sociaux que d’autres. Il est précieux de cultiver les relations avec son entourage, que ce soit avec sa famille, ses amis, ses voisins ou sa communauté.
+
+            Il est parfois possible de s’appuyer sur les amis pour partager les joies et les peines. Il est important de s’encourager à faire un pas vers les autres et à s’ouvrir pour tisser des liens, même si ce n’est pas toujours facile.
+            
+            Le fait de parler et d’écouter permet de mettre de l’ordre dans ses idées, contribue à atténuer les tensions et apporte un certain apaisement. Échanger sur ses soucis amène parfois un nouveau point de vue et fera peut-être apparaître des solutions..</p>
+        </div>
+      </div>
+    </div>
+    
+    </div>
+  </div>
+
+
+  
+ 
+  <script>
+    var slideIndex = 1;
+    showDivs(slideIndex);
+    
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
+    }
+    
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+      }
+      x[slideIndex-1].style.display = "block";  
+    }
+    </script>
+    
+	<?php endwhile; endif; ?>
+  
+
+<?php get_footer(); ?>
